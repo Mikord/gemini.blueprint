e0a7d698afc6fe2f89957a621e18c15f621b14cd
kochetkov <kochetkov@cg.ru>
2015-09-14 11:55:46 +0300

    fix

--- core/src/main/java/org/eclipse/gemini/blueprint/context/support/BlueprintDocumentLoader.java
+++ core/src/main/java/org/eclipse/gemini/blueprint/context/support/BlueprintDocumentLoader.java
@@ -7,9 +7,9 @@
  * http://www.eclipse.org/legal/epl-v10.html and the Apache License v2.0
  * is available at http://www.opensource.org/licenses/apache2.0.php.
  * You may elect to redistribute this code under either of these licenses.
- *
+ * <p>
  * Contributors:
- *   VMware Inc.
+ * VMware Inc.
  *****************************************************************************/
 
 package org.eclipse.gemini.blueprint.context.support;
@@ -33,35 +33,49 @@ import javax.xml.parsers.ParserConfigurationException;
  */
 class BlueprintDocumentLoader extends DefaultDocumentLoader {
 
-	static final String JAXP_SCHEMA_SOURCE = "http://java.sun.com/xml/jaxp/properties/schemaSource";
-	static final String BLUEPRINT_SCHEMA = "http://www.osgi.org/xmlns/blueprint/v1.0.0/blueprint.xsd";
+  static final String JAXP_SCHEMA_SOURCE = "http://java.sun.com/xml/jaxp/properties/schemaSource";
+  static final String BLUEPRINT_SCHEMA = "http://www.osgi.org/xmlns/blueprint/v1.0.0/blueprint.xsd";
 
-	/** logger */
-	private static final Log log = LogFactory.getLog(BlueprintDocumentLoader.class);
+  /**
+   * logger
+   */
+  private static final Log log = LogFactory.getLog(BlueprintDocumentLoader.class);
 
-	@Override
-	protected DocumentBuilderFactory createDocumentBuilderFactory(int validationMode, boolean namespaceAware)
-			throws ParserConfigurationException {
-	  Bundle bundle = FrameworkUtil.getBundle(getClass());
-	  ServiceReference<DocumentBuilderFactory> serviceReference = bundle.getBundleContext().getServiceReference(DocumentBuilderFactory.class);
-	  DocumentBuilderFactory factory = bundle.getBundleContext().getService(serviceReference);
-	  factory.setNamespaceAware(namespaceAware);
+  @Override
+  protected DocumentBuilderFactory createDocumentBuilderFactory(int validationMode, boolean namespaceAware)
+      throws ParserConfigurationException {
+    Bundle bundle = FrameworkUtil.getBundle(getClass());
+    DocumentBuilderFactory factory = null;
 
-	  if(validationMode != 0) {
-	    factory.setValidating(true);
-	  }
-	    if(validationMode == 3) {
-	      factory.setNamespaceAware(true);
-	    }
-//	  DocumentBuilderFactory factory = super.createDocumentBuilderFactory(validationMode, namespaceAware);
-          factory.setAttribute("http://java.sun.com/xml/jaxp/properties/schemaLanguage", "http://www.w3.org/2001/XMLSchema");
-		try {
-			factory.setAttribute(JAXP_SCHEMA_SOURCE, BLUEPRINT_SCHEMA);
-		} catch (IllegalArgumentException ex) {
-			log.warn("Cannot work with attribute " + JAXP_SCHEMA_SOURCE
-					+ " - configurations w/o a schema locations will likely fail to validate", ex);
-		}
+    if (bundle != null && bundle.getBundleContext() != null) {
+      ServiceReference<DocumentBuilderFactory> serviceReference = bundle.getBundleContext().getServiceReference(DocumentBuilderFactory.class);
 
-		return factory;
-	}
+      if (serviceReference != null) {
+        factory = bundle.getBundleContext().getService(serviceReference);
+        factory.setNamespaceAware(namespaceAware);
+
+        if (validationMode != 0) {
+          factory.setValidating(true);
+        }
+        if (validationMode == 3) {
+          factory.setNamespaceAware(true);
+        }
+        factory.setAttribute("http://java.sun.com/xml/jaxp/properties/schemaLanguage", "http://www.w3.org/2001/XMLSchema");
+      }
+    }
+
+    if (factory == null) {
+      factory = super.createDocumentBuilderFactory(validationMode, namespaceAware);
+    }
+
+    try {
+      factory.setAttribute(JAXP_SCHEMA_SOURCE, BLUEPRINT_SCHEMA);
+    }
+    catch (IllegalArgumentException ex) {
+      log.warn("Cannot work with attribute " + JAXP_SCHEMA_SOURCE
+          + " - configurations w/o a schema locations will likely fail to validate", ex);
+    }
+
+    return factory;
+  }
 }
